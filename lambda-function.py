92cd69704e60fabff9eb61f61fd8d77f5db96fce
Behic G <bguven@na.edu>
2022-11-07 12:13:24 -0600

    Update lambda-function.py

--- lambda-function.py
+++ lambda-function.py
@@ -4,13 +4,11 @@ import json
 # define the handler function that the Lambda service will use as an entry point
 def lambda_handler(event, context):
 
-# extract values from the event object we got from the Lambda service
+    # extract values from the event object we got from the Lambda service
     name = event['firstName'] +' '+ event['lastName']
 
-# return a properly formatted JSON object
+    # return a properly formatted JSON object
     return {
     'statusCode': 200,
     'body': json.dumps('Hello from Lambda, ' + name)
     }
-
-    
